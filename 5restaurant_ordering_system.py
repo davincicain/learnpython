436ca723f8fd54b17327b7143d698cd43eadd1fc
davincicain <shaojun.hi@gmail.com>
2025-12-12 15:57:14 +0800

    1212

---
+++ 5restaurant_ordering_system.py
@@ -0,0 +1,39 @@
+#user information
+user1={account:"aaa",password:"111",name:"yi",role:"clerk"}
+user2={account:"bbb",password:"222",name:"er",role:"clerk"}
+user3={account:"ccc",password:"333",name:"san",role:"manager"}
+usersList=[user1,user2,user3]
+
+#menu
+food1={number:"001",name:"chicken",price:"100",inventory:"1",updater:"aaa"}
+food2={number:"002",name:"duck",price:"100",inventory:"1",updater:"aaa"}
+food3={number:"003",name:"fish",price:"100",inventory:"1",updater:"aaa"}
+food4={number:"004",name:"meet",price:"100",inventory:"1",updater:"aaa"}
+food5={number:"005",name:"beer",price:"100",inventory:"1",updater:"aaa"}
+foodlist=[food1,food2,food3,food4,food5]
+
+#login
+def login():
+    pass
+
+#display menu
+def menu():
+    pass
+
+#query food information
+def queryFoodByName():
+    pass
+
+#add food
+def addFood():
+    pass
+
+#delete food
+def deleteFood():
+    pass
+
+#set food information
+def updateFood():
+    pass
+
+#
