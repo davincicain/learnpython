d8a964f454c32dc3364b43468f9137b7db303215
shaojun.hi@gmail.com <shaojun.hi@gmail.com>
2026-01-21 17:59:50 +0800

    2026012102

---
+++ hotkey_and_learn_path.py
@@ -0,0 +1,83 @@
+"""
+ctrl+shift+f: search in the project folder
+shift+shift: search all
+ctrl+f: search in current folder
+ctrl+r: replace in current folder
+ctrl+/: comment the code
+ctrl+shift+u: convert case
+"""
+
+"""
+Learning path
+
+1 MySql
+1.1 index
+1.2 constraint
+1.3 database lock
+1.4 view
+1.5 stored procedure
+1.6 read-write separation
+1.7 database and table sharding
+1.8 database design
+
+2 Redis
+2.1 data type
+2.2 common operations
+2.3 persistence
+2.4 cluster
+2.5 sentinel
+2.6 distributed lock
+2.7 cache
+
+3 project management
+3.1 Git
+3.2 Svn
+3.3 virtual environment
+3.4 configuration
+3.5 cloud server
+
+4 security encryption
+4.1 one-way encryption
+4.2 two-way encryption
+4.3 data desensitization
+4.4 digital signature
+4.5 web security (sql injection, xss)
+
+5 Linux
+5.1 linux order
+5.2 virtual machine operations
+5.3 project deployment
+
+6 message middleware
+6.1 Kafka
+
+7 Web
+7.1 html
+7.2 css
+7.3 js
+7.4 jQuery
+7.5 react
+7.6 vue
+7.7 flask
+7.8 Django
+7.9 interface development
+
+8 web crawler
+8.1 basic net knowledge
+8.2 Scrapy
+8.3 Beautifulsoup
+8.4 anti-crawler
+
+9 automated script
+9.1 third-party modules
+9.2 operating system interaction
+9.2 Selenium
+9.3 Appium
+
+10 data analysis and visualization
+10.1 pandas
+10.2 numpy
+10.3 matplotlib
+10.3 echarts
+10.4 big data knowledge
+"""
